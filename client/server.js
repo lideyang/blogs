7aff078af7c0e0210571e3af90df33e1dee572a6
Lidy Li <lidy.li@hirede.com>
2017-05-24 16:59:46 +0800

    express错误日志

--- client/server.js
+++ client/server.js
@@ -6,10 +6,15 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const favicon = require('serve-favicon')
 const partials = require('express-partials');
+var logger = require('morgan');
+var fs = require('fs')
 const app = express()
 const isDev = true||process.env.NODE_ENV === 'development'
 const defaultPort = isDev? 4000 : 8300
 const port = process.env.PORT || defaultPort
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+// setup the logger
+app.use(logger('combined', {stream: accessLogStream}))
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')))
 app.use(cookieParser());
 app.use(partials());
